1c8cc16b16977972c635188a7f2e229ff79d229b
piotrkusyk <34680@studentwsei.pl>
2020-03-15 16:13:24 +0100

    brak formatowania

--- main.cpp
+++ main.cpp
@@ -5,13 +5,9 @@ using namespace std;
 
 int a, b, c, delta;
 
-/**
- * Główna funkcja
- * @return
- */
 int main() {
 
-    cout << "Program do obliczania funkcji kwadratowej" << endl << endl;
+    cout << "Program do obliczania funkcji kwadratowej"<<endl<<endl;
     cout << "Proszę podać a= ";
     cin >> a;
     cout << "Proszę podać b= ";
@@ -19,21 +15,21 @@ int main() {
     cout << "Proszę podać c= ";
     cin >> c;
 
-    // obliczenie delty
-    delta = b * b - (4 * a * c);
+    delta = b*b-(4*a*c);
 
-    if (delta > 0) {
-        cout << endl;
-        cout << "delta > 0" << endl << endl;
-        cout << "Wynik: x1 = " << (-b - sqrt(delta)) / (2 * a) << " ,x2 = " << (-b + sqrt(delta)) / (2 * a);
-    } else if (delta < 0) {
-        cout << "To równanie funkcji kwadratowej nie posiada rozwiązań.";
-    } else {
-        cout << endl;
-        cout << "delta = 0" << endl << endl;
-        cout << "Wynik: x0 = " << -b / (2 * a);
-    }
+        if (delta > 0){
+        cout<<endl;
+        cout <<"delta > 0"<<endl<<endl;
+        cout <<"Wynik: x1 = "<<(-b-sqrt(delta))/(2*a)<<" ,x2 = "<<(-b+sqrt(delta))/(2*a);
+        }
+        else if (delta < 0){
+            cout << "To równanie funkcji kwadratowej nie posiada rozwiązań.";
+        }
+        else{
+        cout <<endl;
+        cout <<"delta = 0"<<endl<<endl;
+        cout <<"Wynik: x0 = "<<-b/(2*a);
+        }
 
-    //zakończenie programu
     return 0;
 }
