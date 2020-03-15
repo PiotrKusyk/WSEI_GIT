6fc043e33029197b9827192eb8e7583d7d3722c4
piotrkusyk <34680@studentwsei.pl>
2020-03-15 15:46:38 +0100

    program Funkcja_kwadratowa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
+int a, b, c, delta;
+
 int main() {
-    cout << "Hello, World!" <<endl;
+
+    cout << "Program do obliczania funkcji kwadratowej"<<endl<<endl;
+    cout << "Proszę podać a= ";
+    cin >> a;
+    cout << "Proszę podać b= ";
+    cin >> b;
+    cout << "Proszę podać c= ";
+    cin >> c;
+
+    delta = b*b-(4*a*c);
+
+        if (delta > 0){
+        cout<<endl;
+        cout <<"delta > 0"<<endl<<endl;
+        cout <<"Wynik: x1 = "<<(-b-sqrt(delta))/(2*a)<<" ,x2 = "<<(-b+sqrt(delta))/(2*a);
+        }
+        else if (delta < 0){
+            cout << "To równanie funkcji kwadratowej nie posiada rozwiązań.";
+        }
+        else{
+        cout <<endl;
+        cout <<"delta = 0"<<endl<<endl;
+        cout <<"Wynik: x0 = "<<-b/(2*a);
+        }
+
     return 0;
 }
